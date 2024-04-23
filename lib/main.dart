3753b3b9c615cee08971d88acdcc3f9877f673d2
Takiul ISlam Sumon <takiulislam20@gmail.com>
2024-04-24 00:59:55 +0600

    Transection

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transection.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,6 +17,10 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transections> transection = [
+    Transections(id: "f", title: "Bag", amount: 50, date: DateTime.now())
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,14 +29,22 @@ class MyHomePage extends StatelessWidget {
         backgroundColor: Colors.cyan,
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Card(
             child: Text("Chart"),
           ),
           Card(
-            child: Text(
-              "List of Tx",
-              style: TextStyle(color: Colors.white),
+            child: Container(
+              width: double.infinity,
+              child: Center(
+                child: Text(
+                  "List of Tx",
+                  style: TextStyle(
+                      color: Color.fromARGB(255, 255, 253, 253), fontSize: 20),
+                ),
+              ),
             ),
             color: Colors.lightBlue[400],
             elevation: 5,
---
+++ lib/transection.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Transections {
+  String? id;
+  String? title;
+  int? amount;
+  DateTime? date;
+  Transections({
+   @required this.id,@required this.title,@required this.amount,@required this.date});
+}
