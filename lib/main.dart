bce4c8fc2e47a05b8f6970ec46dd39ab1b564e8e
Takiul ISlam Sumon <takiulislam20@gmail.com>
2024-04-25 02:35:51 +0600

    Box Shape Design

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import './transection.dart';
+import 'package:intl/intl_browser.dart';
 
 void main() {
   runApp(MyApp());
@@ -48,7 +50,57 @@ class MyHomePage extends StatelessWidget {
             ),
             color: Colors.lightBlue[400],
             elevation: 5,
-          )
+          ),
+          Column(
+            children: transection.map((tx) {
+              return Card(
+                child: Row(
+                  children: [
+                    Container(
+                      // height: 100,
+                      // width: 100,
+                      margin:
+                          EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 253, 249, 249),
+                        border: Border.all(
+                          color: Colors.blue,
+                          width: 1,
+                        ),
+                      ),
+                      // color: Colors.deepOrange,
+                      child: Text(
+                        '\$' + tx.amount.toString(),
+                        style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.blue,
+                            fontWeight: FontWeight.w600),
+                      ),
+                      padding: EdgeInsets.all(8),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          tx.title.toString(),
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.lightBlue),
+                        ),
+                        Text(
+                          tx.date.toString(),
+                          style: TextStyle(
+                              fontWeight: FontWeight.w200, color: Colors.grey),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                elevation: 12,
+              );
+            }).toList(),
+          ),
         ],
       ),
     );
